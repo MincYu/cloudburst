ee0a6cbfab4819dfe5a92decd8801dce6b165f52
MincYu <myuaj@connect.ust.hk>
2021-02-07 17:08:38 +0800

    update test

--- cloudburst/client/ephe_common.py
+++ cloudburst/client/ephe_common.py
@@ -13,7 +13,7 @@ import os
 from cloudburst.server.benchmarks import utils
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-f_elb = 'af0dfe6431a8e413d9efa9cbb4b33e2f-436026661.us-east-1.elb.amazonaws.com'
+f_elb = 'a3555f3d301f14851b1ff7010b90ffc1-1062765033.us-east-1.elb.amazonaws.com'
 my_ip = '54.226.1.250'
 timeout = 10
 
