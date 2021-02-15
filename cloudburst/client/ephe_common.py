78df977c4ade84d1f3a22e53f64c4c4c7b54ddbf
MincYu <myuaj@connect.ust.hk>
2021-02-15 18:49:36 +0800

    docker file update

--- cloudburst/client/ephe_common.py
+++ cloudburst/client/ephe_common.py
@@ -13,7 +13,7 @@ import os
 from cloudburst.server.benchmarks import utils
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-f_elb = 'a8e920957d21549a694d6d2fd1e1b714-2061365145.us-east-1.elb.amazonaws.com'
+f_elb = 'a2b49e6c4b07b479282e9701e3f724da-374677449.us-east-1.elb.amazonaws.com'
 my_ip = '34.224.41.88'
 timeout = 10
 
