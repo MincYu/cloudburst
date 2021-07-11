9e3e1dcac1a849153c93700a1a07736b4f9250ea
Ubuntu <ubuntu@ip-172-31-19-77.ec2.internal>
2021-07-11 05:40:35 +0000

    update

--- start-client-test.sh
+++ start-client-test.sh
@@ -4,8 +4,7 @@ cd $HYDRO_HOME/cloudburst
 
 IP=`ifconfig eth0 | grep 'inet' | grep -v inet6 | sed -e 's/^[ \t]*//' | cut -d' ' -f2`
 
-elb=$1
-thread=$2
-req_num=$3
+thread=$1
+req_num=$2
 
-python3 -m cloudburst.client.trigger_micro.empty $elb $IP $thread $req_num
+python3 -m cloudburst.client.trigger_micro.empty $FUNCTION_ADDR $IP $thread $req_num
